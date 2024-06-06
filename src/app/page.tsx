e8d84a7113e8cd4e4e19173d375db3c811a89b3c
TI <henrycky_wottikosky@hotmail.com>
2024-06-06 11:03:21 -0300

    :sparkle: feat: Starting Landing Page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import MaxWidthWrapper from "@/components/MaxWidthWrapper";
+import { Check, Star } from "lucide-react";
 import Image from "next/image";
 
 export default function Home() {
@@ -15,10 +16,45 @@ export default function Home() {
                 Your Image on a <span className="bg-green-600 px-2 text-white">Custom</span> Phone Case
               </h1>
               <p className="mt-8 text-lg lg:pr-10 max-w-prose text-center lg:text-left text-balance md:text-wrap">
-                Capture your favorite memories with your own, <span className="font-semibold">one-of-one</span> phone case. ArtDougs allows you to protect yout memories, not just your phone.
+                Capture your favorite images with your own, <span className="font-semibold">one-of-one</span> phone case. ArtDougs allows you to protect not just your phone, but your memories.
               </p>
 
-              <ul className="mt-8 space-y-2 text-left font-medium flex flex-col items-center sm:items-start"></ul>
+              <ul className="mt-8 space-y-2 text-left font-medium flex flex-col items-center sm:items-start">
+                <div className="sspace-y-2">
+                  <li className="flex gap-1.5 items-center text-left">
+                    <Check className="h-5 w-5 shrink-0 text-green-600"/>
+                    High-quality, durable material
+                  </li>
+                  <li className="flex gap-1.5 items-center text-left">
+                    <Check className="h-5 w-5 shrink-0 text-green-600"/>
+                    5 year print guarantee
+                  </li>
+                  <li className="flex gap-1.5 items-center text-left">
+                    <Check className="h-5 w-5 shrink-0 text-green-600"/>
+                    Modern iPhone models supported
+                  </li>
+                </div>
+              </ul>
+
+              <div className="mt-12 flex flex-col sm:flex-row items-center sm:items-start gap-5">
+                <div className="flex -space-x-4">
+                  <img className="inline-block h-10 w-10 rounded-full ring-2 ring-slate-100" src="/users/user-1.png" alt="user image"/>
+                  <img className="inline-block h-10 w-10 rounded-full ring-2 ring-slate-100" src="/users/user-2.png" alt="user image"/>
+                  <img className="inline-block h-10 w-10 rounded-full ring-2 ring-slate-100" src="/users/user-3.png" alt="user image"/>
+                  <img className="inline-block h-10 w-10 rounded-full ring-2 ring-slate-100" src="/users/user-4.jpg" alt="user image"/>
+                  <img className="inline-block object-cover h-10 w-10 rounded-full ring-2 ring-slate-100" src="/users/user-5.jpg" alt="user image"/>
+                </div>
+
+                <div className="flex flex-col justify-between items-center sm:items-start">
+                  <div className="flex gap-0.5">
+                    <Star className="h-4 w-4 text-green-600 fill-green-600"/>
+                    <Star className="h-4 w-4 text-green-600 fill-green-600"/>
+                    <Star className="h-4 w-4 text-green-600 fill-green-600"/>
+                    <Star className="h-4 w-4 text-green-600 fill-green-600"/>
+                    <Star className="h-4 w-4 text-green-600 fill-green-600"/>
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
         </MaxWidthWrapper>
