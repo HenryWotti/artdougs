2a62c76134f933a220eee9215fd41902ce7dc6ac
TI <henrycky_wottikosky@hotmail.com>
2024-06-12 09:01:05 -0300

    :wrench: fix: fixing speling error

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -210,7 +210,7 @@ export default function Home() {
           <div className='mb-12 px-6 lg:px-8'>
             <div className='mx-auto max-w-2xl sm:text-center'>
               <h2 className='order-1 mt-2 tracking-tight text-center text-balance !leading-tight font-bold text-5xl md:text-6xl text-gray-900'>
-                Upsolad your photo and get {' '}
+                Upload your photo and get {' '}
                 <span className='relative px-2 bg-green-600 text-white'>
                   your own case
                 </span>{' '}
